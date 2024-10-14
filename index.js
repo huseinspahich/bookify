0ce8b5344407b2bb99550ece99078e9f87912fcb
Harun Huseinspahic <harunhuseinspahic2001@gmail.com>
2024-10-14 02:16:26 +0200

    Updated

--- index.js
+++ index.js
@@ -20,23 +20,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const API_URL ="https://api.realinspire.tech/v1/quotes/random";
 
-//const API_URL2 = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${API_KEY}`;
-const API_KEY ="AIzaSyA9Cipgnat-CkYhteLf4uz807RJWAaDBKs";
-
-let books = [];
 
 app.get("/", async (req, res) => {
     try {
+        const sort = req.query.sort || 'id';
         const response = await axios.get(API_URL);
         const result = response.data;
         console.log(result);
 
-        const dbQuoteResponse = await db.query("SELECT * FROM books");
+        const books = await db.query(`SELECT * FROM books ORDER BY ${sort} ASC`);
 
         res.render("index.ejs", {
              quote: result[0].content,
              author: result[0].author,
-             books: dbQuoteResponse.rows
+             books: books.rows
             });
     } catch (error) {
         console.error("Failed to make request:", error.message);
@@ -44,6 +41,8 @@ app.get("/", async (req, res) => {
           error: error.message,
         })};
 }); 
+
+
 app.get("/about", async (req, res) => {
     res.render("about.ejs");
 });
@@ -84,21 +83,31 @@ app.post("/addBook", async (req, res) => {
     res.redirect("/");    
 });
 
+app.post("/editBook", async (req, res) => {
+    let title = req.body.title;
+    let author = req.body.author;
+    let rating = req.body.rating;
+    let summary = req.body.summary;
+    let id = req.body.id;
+    const response = await db.query("UPDATE books SET title = $1, authors = $2, rating = $3, summary = $4 WHERE id = $5", [title, author, rating, summary, id]);
+    res.redirect("/");
+})
 
 
 app.post("/delete", async (req, res) => {
     const id = req.body.book_id;
-    
     const response = await db.query("DELETE FROM books WHERE id = $1", [id]);
     res.redirect("/");
-})
+});
+
 app.post("/login", async (req, res) => {
     let message = "Submmited";
     res.render("contact.ejs", { message: message });
 });
-
-
-
+app.post("/sort", async (req, res) => {
+    const sort = req.body.sort || 'id';
+    res.redirect(`/?sort=${sort}`);
+  });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
